76ad5b2b3bbd263d13aaf31ef570b28a0a22e846
陳杰彤 <jtc@ntu.im>
2022-12-24 15:41:04 +0800

    refactor some things

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 import uvicorn
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from fastapi.responses import JSONResponse
-from fastapi.encoders import jsonable_encoder
 from database import database
 from base.do import UserAccount, DietitianAccount, Session, Article
-from util.util import get_dietitian_domain, get_dietitian_time, get_dietitian_name, get_domain_name, get_article_advertiser
+from base.content_formatter import * 
+from util.advertiser import *
+from util.dietitian import *
+from util.domain import *
 
 tags_metadata = [
     { "name": "account", },
@@ -21,22 +23,26 @@ async def get_account(account_id: int):
     row = database.cur.fetchone()
 
     if row is None:
-        res = {
-            "response": {
-                "data": None ,
-                "error": "UserAccountNotFound" },
-        }
-        content = jsonable_encoder(res)
-        return JSONResponse(status_code=404, content=content)
+        return JSONResponse(status_code=404, content=error_content(message="UserAccountNotFound"))
 
-    res = {
-        "response": {
-            "data": UserAccount(id=row[0], name=row[1], gender=row[2], birthday=row[3]) ,
-            "error": None },
-    }
+    data = UserAccount(id=row[0], name=row[1], gender=row[2], birthday=row[3])
+    return JSONResponse(status_code=200, content=data_content(data=data))
 
-    content = jsonable_encoder(res)
-    return JSONResponse(status_code=200, content=content)
+@app.get("/dietitian/{dietitian_id}", tags=["dietitian"], summary="Get Dietitian Account")
+async def get_dietitian(dietitian_id: int):
+    database.cur.execute(f"SELECT id, name, gender, phone_number, introduction, work_unit FROM dietitian_account where id = {dietitian_id}")
+    row = database.cur.fetchone()
+
+    if row is None:
+        return JSONResponse(status_code=404, content=error_content(message="DietitianAccountNotFound"))
+
+    dietitian_id = row[0]
+    domains = await get_dietitian_domain(dietitian_id)
+    available_times = await get_dietitian_time(dietitian_id)
+    data = [DietitianAccount(id=row[0], name=row[1], gender=row[2], domain=domains, available_time=available_times,
+                             phone_number=row[3], introduction=row[4], work_unit=row[5])]
+    
+    return JSONResponse(status_code=200, content=data_content(data=data))
 
 @app.get("/dietitian", tags=["dietitian"], summary="Browse Dietitian Accounts")
 async def browse_dietitian(filter: str=None):
@@ -44,13 +50,7 @@ async def browse_dietitian(filter: str=None):
     rows = database.cur.fetchall()
 
     if rows is None:
-        res = {
-            "response": {
-                "data": None ,
-                "error": "DietitianAccountNotFound" },
-        }
-        content = jsonable_encoder(res)
-        return JSONResponse(status_code=404, content=content)
+        return JSONResponse(status_code=404, content=error_content(message="DietitianAccountNotFound"))
 
     data = []
     for row in rows:
@@ -59,15 +59,8 @@ async def browse_dietitian(filter: str=None):
         available_times = await get_dietitian_time(dietitian_id)
         data.append(DietitianAccount(id=row[0], name=row[1], gender=row[2], domain=domains, available_time=available_times,
                                      phone_number=row[3], introduction=row[4], work_unit=row[5]))
-
-    res = {
-        "response": {
-            "data": data,
-            "error": None },
-    }
-    content = jsonable_encoder(res)
     
-    return JSONResponse(status_code=200, content=content)
+    return JSONResponse(status_code=200, content=data_content(data=data))
 
 @app.get("/account/{account_id}/session", tags=["account"], summary="Get a user's session")
 async def get_session_under_account(account_id: int):
@@ -75,17 +68,9 @@ async def get_session_under_account(account_id: int):
     rows = database.cur.fetchall()
 
     if rows is None:
-        res = {
-            "response": {
-                "data": None ,
-                "error": "SessionUnderUserAccountNotFound" },
-        }
-        content = jsonable_encoder(res)
-        return JSONResponse(status_code=404, content=content)
-    
-    tmpDomainName = "qq"
-    data = []
+        return JSONResponse(status_code=404, content=error_content(message="UserAccountNotFound"))
 
+    data = []
     for row in rows:
         dietitian_id = row[2]
         domain_id = row[3]
@@ -97,14 +82,7 @@ async def get_session_under_account(account_id: int):
                             domain_id=row[3], domain_name=domain_name,
                             session_status=row[4], link=row[5], start_time=row[6], end_time=row[7]))
 
-    res = {
-        "response": {
-            "data": data,
-            "error": None },
-    }
-    
-    content = jsonable_encoder(res)
-    return JSONResponse(status_code=200, content=content)
+    return JSONResponse(status_code=200, content=data_content(data=data))
 
 @app.post("/session", tags=["session"])
 async def add_session(user_id, dietitian_id, domain_id, start_time, end_time):
@@ -127,35 +105,36 @@ async def add_session(user_id, dietitian_id, domain_id, start_time, end_time):
                    domain_id=row[3], domain_name=domain_name,
                    session_status=row[4], link=row[5], start_time=row[6], end_time=row[7])
 
+@app.get("/article/{article_id}", tags=["article"], summary="Get an Article")
+async def get_article(article_id: int):
+    database.cur.execute(f"SELECT id, advertiser_id, post_time, title, context FROM article where id = {article_id}")
+    row = database.cur.fetchone()
+
+    if row is None:
+        return JSONResponse(status_code=404, content=error_content(message="ArticleNotFound"))
+
+    article_id = row[0]
+    advertiser = await get_article_advertiser(article_id)
+    data = Article(id=article_id, advertiser=advertiser, post_time=row[2], title=row[3], context=row[4])
+    
+    return JSONResponse(status_code=200, content=data_content(data=data))
+
+
 @app.get("/article", tags=["article"], summary="Browse Articles")
 async def browse_article(filter: str=None):
     database.cur.execute(f"SELECT id, advertiser_id, post_time, title, context FROM article")
     rows = database.cur.fetchall()
 
     if rows is None:
-        res = {
-            "response": {
-                "data": None ,
-                "error": "ArticleNotFound" },
-        }
-        content = jsonable_encoder(res)
-        return JSONResponse(status_code=404, content=content)
+        return JSONResponse(status_code=404, content=error_content(message="ArticleNotFound"))
 
     data = []
-    print(rows)
     for row in rows:
         article_id = row[0]
         advertiser = await get_article_advertiser(article_id)
         data.append(Article(id=article_id, advertiser=advertiser, post_time=row[2], title=row[3], context=row[4]))
-
-    res = {
-        "response": {
-            "data": data,
-            "error": None },
-    }
-    content = jsonable_encoder(res)
     
-    return JSONResponse(status_code=200, content=content)
+    return JSONResponse(status_code=200, content=data_content(data=data))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="localhost", port=8000)
